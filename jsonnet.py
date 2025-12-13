592d0f79cab577003602633eba86bc368cb31502
tazz <tazz@tazz.cloud>
2025-12-13 13:29:42 -0500

    feat: :sparkles: get script to work properly w/ jsonnet and some customizations

--- jsonnet.py
+++ jsonnet.py
@@ -3,17 +3,64 @@ from pprint import PrettyPrinter
 
 __version__ = "0.0.1"
 
+class JsonnetResult:
+  def __init__(self, out, config):
+    if os.path.isdir(out) or (out.endswith("/") and not os.path.isfile(out)):
+      self._multi = out
+      print(f"multi: {out}")
+    else:
+      raise ValueError(f"failed to determine output of jsonnet: {out}")
+    if isinstance(config, str):
+      self._source = str
+    elif not "source" in config:
+      raise ValueError(f"failed to find source for {config}")
+    self._source = config["source"]
+    self._config = config
+    
+    self._ext_strs = []
+    if "vars_from_env" in config:
+      for v in config["vars_from_env"]:
+        self._ext_strs.append(v)
+    if "vars" in config:
+      for k,v in config["vars"].items():
+        self._ext_strs.append(f"{k}=\"{v}\"")
+
+  def is_plain_text(self):
+    return self._config.plain_text if "plain_text" in self._config else True
+
+  def is_multi(self):
+    return self._multi != None
+
+  def get_command(self, extras = None):
+    command = []
+    command.append('jsonnet')
+    command.append('--create-output-dirs')
+    if self.is_plain_text():
+      command.append('-S')
+    if self.is_multi():
+      command.append('-m')
+      command.append(self._multi)
+    print("ex strs")
+    for ex in self._ext_strs:
+      command.append("--ext-str")
+      command.append(ex)
+    command.append(self._source)
+    if extras != None:
+      command.append(extras)
+    return command
+
 class DotbotJsonnet(dotbot.Plugin):
   def can_handle(self, directive):
     """Flag whether or not this plugin supports the given *directive*."""
     valid = directive == "jsonnet"
-    if valid:
+    if not valid:
       self._log.debug(f"The Jsonnet plugin doesn't support the `{directive}` directive")
     return valid
 
   def handle(self, directive, data):
-    if directive != self._directive:
-      raise ValueError("jsonnet cannot handle `%s` directive" % directive)
+    if not self.can_handle(directive):
+      self._log.error(f'cannot handle the `{directive}` directive')
+      return False
 
     try:
       self._validate(data)
@@ -21,42 +68,25 @@ class DotbotJsonnet(dotbot.Plugin):
       self._log.error(error.args[0])
       return False
 
-    multi: str = data.get("milti", "generated/")
-    sources: str|list = data.get("sources", [])
-    if isinstance(sources, str):
-      return self._run_jsonnet(sources, multi)
-    for source in sources:
-      self._run_jsonnet(source, multi)
+    for k,v in data.items():
+      try:
+        result = JsonnetResult(k, v)
+        command = result.get_command()
+        self._run_command(command)
+      except Exception as ex:
+        print(f"failed to process jsonnet file `{k}` with config `{config}`: {ex}")
     return True
-
-  def _run_jsonnet(self, source, multi = None, plain_text = True, extras = None):
-    try:
-      command = []
-      command.append('jsonnet')
-      command.append('-c')
-      if plain_text:
-        command.append('-S')
-      if multi:
-        command.append('-m')
-        command.append(multi)
-      command.append(sources)
-      if extras:
-        command.append(extras)
-      return self._run_command(' '.join(command))
-    except:
-      return False
-
-  def _run_command(self, command):
-    try:
-      if isinstance(command, list):
-        command = ' '.join(command)
-      elif not isinstance(command, str):
-        raise ValueError(f'command `{command}` is not an instance of a string or list')
-      subprocess.run([command], shell=True, check=True)
-      return True
-    except subprocess.CalledProcessError:
-      self._log.error('jsonnet command failed.')
-      return False
 
   def _validate(self, data):
     pass
+
+  def _run_command(self, command):
+    try:
+      if not isinstance(command, list):
+        command = [command]
+      print(f'running: {command}')
+      subprocess.run([' '.join(command)], shell=True, check=True)
+      return True
+    except Exception as error:
+      print(f"failed to run jsonnet: {error}")
+      return False
